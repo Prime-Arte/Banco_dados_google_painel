add81588e8e40c43104f6472e7e990f4baf17cc8
Prime Arte <103965817+Prime-Arte@users.noreply.github.com>
2022-05-19 09:40:35 -0300

    Add files via upload

--- index.js
+++ index.js
@@ -56,9 +56,10 @@ app.get('/', (req, res) => { // para renderizar a home;
 
 app.post('/CREATE/TABLE', async (req, res, next) => {
 
+  const database = req.body.database
   const table = req.body.table;
   console.log(table);
-  const sql = `CREATE TABLE ${table} (ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT);`
+  const sql = `CREATE TABLE ${database}.${table} (ID INT PRIMARY KEY NOT NULL AUTO_INCREMENT);`
 
   poll.getConnection((error, conn) => {
     if (error) {
@@ -85,9 +86,10 @@ app.post('/CREATE/TABLE', async (req, res, next) => {
 //*Drop Tabela
 app.post('/DROP/TABLE', async (req, res, next) => {
 
-  const database = req.body.database;
+  const database = req.body.database
+  const table = req.body.table;
 
-  const sql = `DROP TABLE ${database}`
+  const sql = `DROP TABLE ${database}.${table}`
 
   poll.getConnection((error, conn) => {
     if (error) {
@@ -113,11 +115,12 @@ app.post('/DROP/TABLE', async (req, res, next) => {
 //*Inserir coluna
 app.post('/ADD/COLUN', async (req, res, next) => {
 
+  const database = req.body.database
   const table = req.body.table;
   const colun = req.body.colun;
   const tipo_dado = req.body.tipo_dado;
 
-  const sql = `ALTER TABLE ${table} ADD ${colun} ${tipo_dado}`
+  const sql = `ALTER TABLE ${database}.${table} ADD ${colun} ${tipo_dado}`
 
   poll.getConnection((error, conn) => {
     if (error) {
@@ -143,10 +146,11 @@ app.post('/ADD/COLUN', async (req, res, next) => {
 //*Apagar coluna
 app.post('/DROP/COLUN', async (req, res, next) => {
 
+  const database = req.body.database
   const table = req.body.table;
   const colun = req.body.colun;
 
-  const sql = `ALTER TABLE ${table} DROP COLUMN ${colun}`
+  const sql = `ALTER TABLE ${database}.${table} DROP COLUMN ${colun}`
 
   poll.getConnection((error, conn) => {
     if (error) {
@@ -172,12 +176,13 @@ app.post('/DROP/COLUN', async (req, res, next) => {
 //*Modificar Nome da coluna
 app.post('/MODF/COLUN', async (req, res, next) => {
 
+  const database = req.body.database
   const table = req.body.table;
   const colun_modifica = req.body.colun_modifica;
   const nova_colun = req.body.nova_colun;
   const tipo_dado = req.body.tipo_dado;
 
-  const sql = `ALTER TABLE ${table} CHANGE ${colun_modifica} ${nova_colun} ${tipo_dado};`
+  const sql = `ALTER TABLE ${database}.${table} CHANGE ${colun_modifica} ${nova_colun} ${tipo_dado};`
 
   poll.getConnection((error, conn) => {
     if (error) {
@@ -199,19 +204,16 @@ app.post('/MODF/COLUN', async (req, res, next) => {
 
 });
 
-
-
-
-
 //* resgatando dados especificos
 app.get('/BUSCAR/ESPECF/', (req, res) => {
 
-  const table = req.query['table']
-  const colun = req.query['colun']
-  const colun_especifico = req.query['colun_especifico']
-  const dado = req.query['dado']
+  const database = req.query["database"]
+  const table = req.query["table"]
+  const colun = req.query["colun"]
+  const colun_especifico = req.query["colun_especifico"]
+  const dado = req.query["dado"]
 
-  const sql = `SELECT ${colun} FROM ${table} WHERE ${colun_especifico} = '${dado}'`
+  const sql = `SELECT ${database}.${colun} FROM ${table} WHERE ${colun_especifico} = '${dado}'`
 
 
   poll.getConnection((error, conn) => {
@@ -240,8 +242,9 @@ app.get('/BUSCAR/ESPECF/', (req, res) => {
 //* resgatando dados todos dados da coluna
 app.get('/BUSCAR/', (req, res) => {
 
-  const table = req.query[table]
-  const sql = `SELECT * FROM ${table}`
+  const database = req.query["database"]
+  const table = req.query["table"]
+  const sql = `SELECT * FROM ${database}.${table}`
 
   poll.getConnection((error, conn) => {
     if (error) {
@@ -265,22 +268,19 @@ app.get('/BUSCAR/', (req, res) => {
 //*Input de dados
 app.post('/INPUT/', (req, res, next) => {
 
+  const database = req.body.database
   const table = req.body.table;
   const {
     colun1,
-    colun2,
-    colun3,
-    colun4
+
   } = req.body;
 
   const {
     dado_input1,
-    dado_input2,
-    dado_input3,
-    dado_input4
+
   } = req.body;
 
-  const sql = `INSERT INTO ${table} (${colun1},${colun2},${colun3},${colun4}) VALUE ('${dado_input1}','${dado_input2}','${dado_input3}','${dado_input4}')`
+  const sql = `INSERT INTO ${database}.${table} (${colun1}) VALUE ('${dado_input1}')`
 
   poll.getConnection((error, conn) => {
     if (error) {
@@ -303,14 +303,15 @@ app.post('/INPUT/', (req, res, next) => {
 })
 
 //*Voltar descrição da tabela
-app.get('/DESC/TABLE/:table', function (req, res) {
+app.get('/DESC/TABLE/', function (req, res) {
 
-  const table = req.params.table
+  const database = req.query["database"]
+  const table = req.query["table"]
   console.log(table)
 
 
 
-  const sql = `DESC ${table}`
+  const sql = `DESC ${database}.${table}`
 
   poll.getConnection((error, conn) => {
     if (error) {
@@ -334,6 +335,7 @@ app.get('/DESC/TABLE/:table', function (req, res) {
 app.delete('/DELETE/',
   function (req, res) {
 
+    const database = req.body.database
     const table = req.body.table
     const {
       colun1,
@@ -345,7 +347,7 @@ app.delete('/DELETE/',
 
     } = req.body;
 
-    const sql = `DELETE FROM ${table} WHERE ${colun1}='${dado_input1}'`
+    const sql = `DELETE FROM ${database}.${table} WHERE ${colun1}='${dado_input1}'`
 
     poll.getConnection((error, conn) => {
       if (error) {
@@ -368,7 +370,7 @@ app.delete('/DELETE/',
 
 app.patch('/UPDATE/',
   function (req, res) {
-
+    const database = req.body.database
     const table = req.body.table
     const {
       coluna_chave,
@@ -378,7 +380,7 @@ app.patch('/UPDATE/',
       dado_ser_modification,
     } = req.body;
 
-    const sql = `UPDATE ${table} SET ${coluna_chave} = '${dado_ser_modification}' WHERE ${coluna_chave} = '${dado_chave}';`
+    const sql = `UPDATE ${database}.${table} SET ${coluna_chave} = '${dado_ser_modification}' WHERE ${coluna_chave} = '${dado_chave}';`
 
     poll.getConnection((error, conn) => {
       if (error) {
